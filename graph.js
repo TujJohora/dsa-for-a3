1ba531245620b057d2d61488cf36069a30fdf504
shohan-pherones <shohan.sub.56@gmail.com>
2023-01-22 22:12:46 +0600

    graph added

---
+++ graph.js
@@ -0,0 +1,97 @@
+// adjacency matrix (O(n))
+const matrix = [
+  [0, 1, 0],
+  [1, 0, 1],
+  [0, 1, 0],
+];
+
+// 2d matrix
+// 0 --> no connection
+// 1 --> connected
+
+console.log(matrix[0][1]); // 1
+console.log(matrix[0][2]); // 0
+console.log(matrix[1][2]); // 1
+console.log(matrix[0][0]); // 0
+
+// adjacency list (O(1))
+adjacencyList = {
+  A: ["B"],
+  B: ["A", "C"],
+  C: ["B"],
+};
+
+console.log(adjacencyList["A"]);
+
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = new Set(); // more efficient
+    }
+  }
+
+  addEdge(vertex1, vertex2) {
+    if (!this.adjacencyList[vertex1]) {
+      this.addVertex(vertex1);
+    }
+    if (!this.adjacencyList[vertex2]) {
+      this.addVertex(vertex2);
+    }
+
+    this.adjacencyList[vertex1].add(vertex2);
+    this.adjacencyList[vertex2].add(vertex1);
+  }
+
+  display() {
+    for (let vertex in this.adjacencyList) {
+      console.log(vertex + " --> " + [...this.adjacencyList[vertex]]);
+    }
+  }
+
+  hasEdge(vertex1, vertex2) {
+    return (
+      this.adjacencyList[vertex1].has(vertex2) &&
+      this.adjacencyList[vertex2].has(vertex1)
+    );
+  }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].delete(vertex2);
+    this.adjacencyList[vertex2].delete(vertex1);
+  }
+
+  // time depends on the list
+  removeVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      return null;
+    } else {
+      // remove edges
+      for (let adjacencyVertex of this.adjacencyList[vertex]) {
+        this.removeEdge(vertex, adjacencyVertex);
+      }
+
+      // delete vertex
+      delete this.adjacencyList[vertex];
+    }
+  }
+}
+
+const graph = new Graph();
+
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+
+graph.addEdge("A", "B");
+graph.addEdge("B", "C");
+
+// console.log(graph.hasEdge("A", "C"));
+
+// graph.removeEdge("C", "B");
+
+graph.removeVertex("A");
+graph.display();
